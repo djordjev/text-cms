dd98c5fa812dc1c9ec413bf0895bfa5ae626a7d6
Djordje Vukovic <djordje@gyorgy.io>
2024-04-21 16:32:47 +0200

    Graphql (#39)

--- cpanel/test/setup-test-env.ts
+++ cpanel/test/setup-test-env.ts
@@ -5,5 +5,6 @@ import { installGlobals } from '@remix-run/node';
 process.env.SESSION_SECRET = 'secret';
 process.env.REDIS_URL = 'redis';
 process.env.API_URL = 'api_endpoint';
+process.env.PROTOCOL = 'rest';
 
 installGlobals();
